8e8a5978689b1a391038923a6984061617c2c3b1
Amanu Nur A <amanunura@Amanus-MacBook-Pro.local>
2021-10-03 23:48:17 +0700

    add vue js

---
+++ src/components/Footer.vue
@@ -0,0 +1,72 @@
+<template>
+  <footer class="footer">
+    <hr class="driver" />
+    <div class="container mt-4">
+      <div class="row">
+        <div class="col-12 col-lg-8">
+          <a class="navbar-brand" href="/">
+            <img src="src/image/brand.png" width="120" alt="" />
+          </a>
+          <p class="text-footer">SMK Muhammadiyah 7 Gondanglegi - Malang</p>
+          <p class="footer-address">
+            Jl. KH. Ahmad Dahlan No.20, Dusun Krajan, Putat Kidul, Kec.
+            Gondanglegi, Malang, Jawa Timur 65174
+          </p>
+        </div>
+        <div class="col-6 col-lg-2 mt-lg-3">
+          <p class="title-link">Links</p>
+          <ul class="list-group list-group-flush mt-2">
+            <li class="list-item">
+              <a href="#/">Home</a>
+            </li>
+            <li class="list-item">
+              <a href="#informasi">Informasi</a>
+            </li>
+            <li class="list-item">
+              <a href="#lowongan_pekerjaan">Lowongan Kerja</a>
+            </li>
+            <li class="list-item">
+              <a href="#bkk">BKK</a>
+            </li>
+            <li class="list-item">
+              <a href="#login">Login</a>
+            </li>
+            <li class="list-item">
+              <a href="#registrasi_siswa">Registrasi Siswa</a>
+            </li>
+            <li class="list-item">
+              <a href="#">Registrasi Perusahaan</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-6 col-lg-2 mt-lg-3">
+          <p class="title-link">Contact</p>
+          <ul class="list-group list-group-flush mt-2">
+            <li class="list-item">
+              <a href="#">Whatsapp</a>
+            </li>
+            <li class="list-item">
+              <a href="#">Instagram</a>
+            </li>
+            <li class="list-item">
+              <a href="#">Twitter</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <p class="copyright">
+            Copyright &COPY; SMK Muhammadiyah 7 Gondanglegi | 2021
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+      <a class="navbar-brand" href="#/">
+        <img src="image/brand.png" width="120" alt="" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto">
+          <a class="nav-link active" aria-current="page" href="#/">Home</a>
+          <a class="nav-link" href="#informasi">Informasi</a>
+          <a class="nav-link" href="#lowongan_pekerjaan">Lowongan pekerjaan</a>
+          <a class="nav-link" href="#bkk">BKK</a>
+          <a class="nav-link" href="#login">Login</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
